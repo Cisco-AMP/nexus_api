7fad5c1cdbeff71765167d5dbddf87b753385222
Francis Levesque <francis.d.levesque@gmail.com>
2020-04-30 17:11:31 +0200

    Add optional arguments to move components method

--- lib/endpoints/staging.rb
+++ lib/endpoints/staging.rb
@@ -2,8 +2,11 @@ module NexusAPI
   class API
     # POST /service/rest/v1/staging/delete
     # POST /service/rest/v1/staging/move/{destination}
-    def move_components_to(destination:, tag:)
-      @connection.post(endpoint: "staging/move/#{destination}?tag=#{tag}")
+    def move_components_to(destination:, tag:, source: nil, keyword: nil)
+      endpoint = "staging/move/#{destination}?tag=#{tag}"
+      endpoint += "&repository=#{source}" unless source.nil?
+      endpoint += "&q=#{keyword}" unless keyword.nil?
+      @connection.post(endpoint: endpoint)
     end
   end
 end
