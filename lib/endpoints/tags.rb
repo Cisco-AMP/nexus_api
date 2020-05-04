0ed91b78616c913bd3617ae667f7ff4fa11c38ce
Francis Levesque <francis.d.levesque@gmail.com>
2020-05-04 19:33:33 +0200

    Add more search parameters to tag and untag functions

--- lib/endpoints/tags.rb
+++ lib/endpoints/tags.rb
@@ -21,24 +21,47 @@ module NexusAPI
     end
 
     # POST /service/rest/v1/tags/associate/{tagName}
-    def associate_tag(name:, sha1:, repository: nil)
+    def associate_tag(name:, sha1: nil, repository: nil, tag: nil)
       repository ||= @team_config.tag_repository
-      search_query =
-        "?"\
-        "wait=true&"\
-        "repository=#{repository}&"\
-        "sha1=#{sha1}"
+      if sha1.nil? && repository.nil? && tag.nil?
+        puts_error(__method__)
+        return false
+      end
+
+      search_query = "?wait=true"
+      search_query += "&sha1=#{sha1}" unless sha1.nil?
+      search_query += "&repository=#{repository}" unless repository.nil?
+      search_query += "&tag=#{tag}" unless tag.nil?
+
       @connection.post(endpoint: "tags/associate/#{name}" + search_query)
     end
 
     # DELETE /service/rest/v1/tags/associate/{tagName}
-    def delete_associated_tag(name:, sha1:, repository: nil)
+    def delete_associated_tag(name:, sha1: nil, repository: nil, tag: nil)
       repository ||= @team_config.tag_repository
-      search_query =
-        "?"\
-        "repository=#{repository}&"\
-        "sha1=#{sha1}"
-      @connection.delete(endpoint: "tags/associate/#{name}" + search_query)
+      parameters = {}
+      parameters['sha1'] = sha1 unless sha1.nil?
+      parameters['repository'] = repository unless repository.nil?
+      parameters['tag'] = tag unless tag.nil?
+      if parameters.empty?
+        puts_error(__method__)
+        return false
+      end
+
+      search_query = parameters.map do |parameter, value|
+        "#{parameter}=#{value}"
+      end.join("&")
+
+      @connection.delete(endpoint: "tags/associate/#{name}?" + search_query)
+    end
+
+
+    private
+
+    def puts_error(method)
+      puts "ERROR: NexusAPI::API::#{method}() requires AT LEAST one optional parameter to"
+      puts "       be set otherwise ALL assets in Nexus will be tagged. If this is desired"
+      puts "       please open an issue or a PR to add a new method to handle this case."
     end
   end
 end
